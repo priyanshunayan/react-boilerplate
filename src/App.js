b26dfa6942820eb63a9041449a570055c482352d
Priyanshu Nayan <priyanshunayan456@gmail.com>
2021-03-04 14:22:14 +0530

    react-boilerplate with all the necessary tooling

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { render } from "react-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Page from "./Page";
+
+const App = () => {
+  return(
+    <Router>
+      <Link to="/page">New Page </Link>
+      <Link to="/"> Home</Link>
+      <Switch>
+        <Route path="/page">
+          <Page />
+        </Route>
+        { /* This should be at the end */ }
+        <Route path="/">
+          <h1>Welcome to Priyanshu's boilerplate</h1>
+        </Route>
+      </Switch>
+    </Router>
+  ) 
+};
+
+render(<App />, document.getElementById("root"));
---
+++ src/Page.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Page = () => {
+    return (
+        <div>
+            <h1>This is a new page</h1>
+            <p>This uses React router by default</p>       
+        </div>
+    )
+}
+
+export default Page;
